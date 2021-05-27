e62699ce43644cbc12af92b6d89ccb40b3fc0614
xygao <xygao0703@gmail.com>
2021-05-28 01:15:39 +0900

    inference package

--- generative_inpainting/setup.py
+++ generative_inpainting/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="generative_inpainting",
     version="0.0.1",
     author="Xiangyi Gao",
     description="inpainting image hole",
-    packages=["generative_inpainting"],
+    packages=find_packages(),
     install_requires=[
         "torch==1.8.1",
         "torchvision==0.9.1",
