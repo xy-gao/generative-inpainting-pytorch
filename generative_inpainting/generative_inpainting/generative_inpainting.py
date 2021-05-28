534f8cce2c56ea4156ff43b801518ea8a02c429f
xygao <xygao0703@gmail.com>
2021-05-29 00:49:22 +0900

    inference package

--- generative_inpainting/generative_inpainting/generative_inpainting.py
+++ generative_inpainting/generative_inpainting/generative_inpainting.py
@@ -40,12 +40,11 @@ class GenerativeInpainter:
         self.x = default_loader(image_path)
         self.mask = default_loader(mask_path)
 
-    def _proc(self):
-        image_shape = list(self.x.size)
-        x = transforms.Resize(image_shape)(self.x)
-        x = transforms.CenterCrop(image_shape)(x)
-        mask = transforms.Resize(image_shape)(self.mask)
-        mask = transforms.CenterCrop(image_shape)(mask)
+    def _proc(self, image_size):
+        x = transforms.Resize(image_size)(self.x)
+        x = transforms.CenterCrop(image_size)(x)
+        mask = transforms.Resize(image_size)(self.mask)
+        mask = transforms.CenterCrop(image_size)(mask)
         x = transforms.ToTensor()(x)
         mask = transforms.ToTensor()(mask)[0].unsqueeze(dim=0)
         x = normalize(x)
@@ -62,7 +61,7 @@ class GenerativeInpainter:
     def _write(self, output_path):
         vutils.save_image(self.inpainted_result, output_path, padding=0, normalize=True)
 
-    def __call__(self, image_path, mask_path, output_path):
+    def __call__(self, image_path, mask_path, output_path, image_size=[512, 512, 3]):
         self._read(image_path, mask_path)
-        self._proc()
+        self._proc(image_size)
         self._write(output_path)
